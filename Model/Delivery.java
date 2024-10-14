d7a39e79752278f25dda9d631b52209c86a90c53
lzhang <zlz200210@gmail.com>
2024-10-14 23:53:13 +0200

    Model

---
+++ Model/Delivery.java
@@ -0,0 +1,31 @@
+import java.sql.Time;
+import java.util.Date;
+
+public class Delivery {
+    private Location pickupLocation;
+    private Location deliveryLocation;
+    private Time pickupTime;
+    private Time deliveryTime;
+    private Courier courier;
+
+    public Location getPickupLocation() {
+        return pickupLocation;
+    }
+
+    public Location getDeliveryLocation() {
+        return deliveryLocation;
+    }
+
+    public Time getPickupTime() {
+        return pickupTime;
+    }
+
+    public Time getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public Courier getCourier() {
+        return courier;
+    }
+}
+
---
+++ Model/Intersection.java
@@ -0,0 +1,12 @@
+public class Intersection {
+    private double latitude;
+    private double longitude;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+}
