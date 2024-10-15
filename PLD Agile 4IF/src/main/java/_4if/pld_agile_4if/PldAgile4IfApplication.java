71a6fd92f1a3e01cd6b48737d70b4af19c5223e4
Antoine Aubut <aubutantoine@gmail.com>
2024-10-15 17:57:38 +0200

    Creation du bon environnement de travail

---
+++ PLD Agile 4IF/src/main/java/_4if/pld_agile_4if/PldAgile4IfApplication.java
@@ -0,0 +1,13 @@
+package _4if.pld_agile_4if;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PldAgile4IfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PldAgile4IfApplication.class, args);
+	}
+
+}
