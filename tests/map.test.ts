27b60b779fa276f78185e806a8caea82dbdbd263
Adam <aallaz.pro@gmail.com>
2024-11-13 14:11:23 +0100

    feat(e2e): new e2e tests

---
+++ tests/map.test.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+import path from 'path';
+
+test('load a map', async ({ page }) => {
+  test.setTimeout(15000)
+  await page.goto('localhost:8080/');
+
+  const fileChooserPromise = page.waitForEvent('filechooser');
+
+  await page.getByRole('button', { name: 'Import .xml delivery map file' }).click()
+
+  const fileChooser = await fileChooserPromise;
+  await fileChooser.setFiles(path.join(__dirname, 'petitPlan.xml'));
+
+  await page.getByRole('button', { name: 'Confirm Map' }).click()
+  await page.waitForTimeout(5000)
+
+  await test.step('load a tour', async() => {
+    test.setTimeout(15000)
+
+    const fileChooserPromise = page.waitForEvent('filechooser');
+
+    page.getByRole('button', { name: 'Import .xml delivery tour file' }).click()
+
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles(path.join(__dirname, 'petitPlan.xml'));
+
+    await page.getByRole('button', { name: 'Confirm Tour' }).click()
+    await page.waitForTimeout(5000)
+  });
+});
